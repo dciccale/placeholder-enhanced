853adc327e28de91f6c092d4b202b990aff50208
dciccale <dciccale@gmail.com>
2011-06-21 16:17:31 +0200

    reestablish placeholder values just for the demo

--- js/demo.js
+++ js/demo.js
@@ -5,5 +5,8 @@ $(function() {
 	form.submit(function(e) {
 		e.preventDefault();
 		result.html(form.serialize());
+		
+		// just for the demo, reestablish placeholder
+		$("input:text[placeholder], textarea[placeholder]").trigger("blur.placeholder");
 	});
 });
